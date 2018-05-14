b9bd6a3701727803982c21e1fb2d6c1aabb01e89
Влад Тягло <acc4diffgame@gmail.com>
2018-05-14 15:31:11 +0300

    animated wiki

--- wiki.js
+++ wiki.js
@@ -4,10 +4,8 @@ function myFunction(){
 	
 		if(counter==0){
 			page.style.animation = "rotatePages 1.5s ease-in-out forwards";
-			// alert("first step");
 			counter+=1;
 		}else if(counter==1){
-			// alert("second step");
 			page.style.animation = "rotatePagesRev 1.5s ease-in-out forwards";
 			counter-=1;
 		}
@@ -17,8 +15,17 @@ document.addEventListener("click", function(e){
         e.preventDefault();
         if(e.target.hasAttribute("name")){
 			// setTimeout(document.body.style.background="#000000c9", 3000);
+			document.getElementById("infoPage").style.display="block";
 			document.getElementById("infoPage").style.animation = "SummonPages .5s ease-in-out forwards";
         }
     });
+
+function HidePage(){
+	document.getElementById("infoPage").style.animation = "SummonPagesRev .5s ease-in-out forwards";
+	function PageNone(){
+		document.getElementById("infoPage").style.display="none";
+	}
+	setTimeout(PageNone,500);
+}
 	
 	
